81808ab89b6fd76596bacc7288079e5a2942ee3f
Kyle Conlon <kyleconlon13@gmail.com>
2019-03-06 04:06:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function deleteFromObjectByKey(recipes, key) {
 }
 
 function destructivelyDeleteFromObjectByKey(recipes, key) {
-  delete recipes.key;
+  delete recipes[key];
   return recipes;
 }
 
