e1204712c4ee96d3a113d2874e4353a9a5febe00
Kyle Conlon <kyleconlon13@gmail.com>
2019-03-06 03:44:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ var recipes = {
   
 };
 
-//function updateObjectWithKeyAndValue(recipes, key, value) {
-  //recipes[key] = value;
-  //return recipes;
-//}
-
 function updateObjectWithKeyAndValue(recipes, key, value) {
   return Object.assign({}, recipes, {[key]: value})
 }
 
+function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value) {
+  recipes[key] = value;
+  return recipes;
+}
